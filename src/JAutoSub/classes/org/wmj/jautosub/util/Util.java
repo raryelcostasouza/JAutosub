456735239c85611e9400896e7349526f9d955b02
raryelcostasouza <raryel.costa@gmail.com>
2018-07-05 11:27:32 +0700

    License header added

--- src/JAutoSub/classes/org/wmj/jautosub/util/Util.java
+++ src/JAutoSub/classes/org/wmj/jautosub/util/Util.java
@@ -1,3 +1,19 @@
+/*
+   (C) 2018 Raryel C. Souza
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.wmj.jautosub.util;
 
 import java.io.File;
@@ -9,15 +25,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Adhipanyo
- */
 public class Util
 {
 
